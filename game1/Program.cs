e257eac1c4d0a99819b99eb9e2e0f3f48164f300
PTKRTK <51212308+PTKRTK@users.noreply.github.com>
2020-11-12 20:30:53 +0100

    Add files via upload

---
+++ game1/Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualBasic;
+using System;
+
+namespace game1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] wyswietlanie = new int[100, 100];
+            Wyswietlanie wyswietlanie_ = new Wyswietlanie();
+            wyswietlanie_.Wyswietl(wyswietlanie);
+//xd
+            
+
+        }
+
+        
+    }
+}
---
+++ game1/Wyswietlanie.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace game1
+{
+    class Wyswietlanie
+    {
+        //szerokosc okna
+        private static int sz_o=100;
+        //wysokosc okna
+        private static int w_o=100;
+        public void Wyswietl(int[,] wyswietlanie)
+        { 
+            tlo(wyswietlanie);
+            Wypisz(wyswietlanie);
+
+        
+        }
+
+        private static void tlo(int[,] wyswietlanie)
+        {
+            for (int i = 0; i < w_o ; i++)
+            {
+                for (int j = 0; j < sz_o; j++)
+                {
+                    wyswietlanie[i, j] = j;
+                }
+            }
+        }
+
+        private static void Wypisz(int[,] wyswietlanie)
+        {
+          for (int i = 0; i < w_o ; i++)
+            {
+                for (int j = 0; j < sz_o; j++)
+                {
+                    Console.Write(wyswietlanie[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+    }
+}
